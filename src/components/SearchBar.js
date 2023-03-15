4c2bf5576e3025d02de54f6e151d953735de322d
Reydescel Herrera <reydi1@hotmail.com>
2023-03-15 14:49:00 -0600

    Adding SearchBar Component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,29 +1,35 @@
 import { useState } from "react";
+import { useDispatch } from 'react-redux';
+import { useNavigate } from "react-router-dom";
 import { MagnifyingGlassIcon } from '@heroicons/react/24/solid';
 
-import axios from 'axios';
+import { setPokemon } from '../features/pokemon/pokemonSlice';
+import { getPokemon } from "../services/Pokemon";
 
 function SearchBar() {
     const [isFocused, setIsFocused] = useState(false);
     const [searchValue, setSearchValue] = useState('');
-    const [pokeImg, setPokeImg] = useState('');
     const [error, setError] = useState('');
+
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
 
     const handleSearchValue = (event) => {
         setSearchValue(event.target.value);
     }
 
-    const handleEnter = (event) => {
+    const handleEnter = async (event) => {
         if (event.key === 'Enter') {
             if (searchValue !== '' && searchValue.length !== 0) {
                 console.log('Search Pokémon:', searchValue);
+                let response = await getPokemon(searchValue);
 
-                axios.get(`https://pokeapi.co/api/v2/pokemon/${searchValue}/`)
-                    .then((response) => {
-                        setError('');
-                        // setPokeImg(response.data.sprites.other["official-artwork"].front_default);
-                    })
-                    .catch((err) => setError('Pokémon not found'));
+                if (response === undefined) {
+                    setError('The Pokémon was not Found');
+                } else {
+                    dispatch(setPokemon(response));
+                    navigate('/entry');
+                }
             } else {
                 setError('Type the Pokémon to search');
             }
@@ -31,22 +37,41 @@ function SearchBar() {
     }
 
     return (
-        <div className="bg-white relative flex items-center justify-around relative mt-3 rounded-lg w-2/3 p-1 md:w-1/2 lg:w-1/3">
+        <div 
+            className="
+                bg-white relative flex items-center justify-around mt-3 rounded-lg w-2/3 p-1 
+                md:w-1/2 lg:w-1/3
+            "
+        >
             <input
                 type="text"
-                className={ isFocused ? 'py-1 px-2 outline-none w-full' : 'py-1 px-5 outline-none w-5/6' }
                 placeholder="Search"
+                className={
+                    isFocused 
+                        ? 'py-1 px-2 outline-none w-full'
+                        : 'py-1 px-5 outline-none w-5/6'
+                }
                 onChange={ handleSearchValue }
                 onKeyDown={ handleEnter }
                 onFocus={ () => setIsFocused(true) }
                 onBlur={ () => setIsFocused(false) }
             />
 
-            <div className={ isFocused ? 'hidden' : 'text-center w-1/8'}>
+            <div className={
+                isFocused 
+                    ? 'hidden' 
+                    : 'text-center w-1/8'
+                }
+            >
                 <MagnifyingGlassIcon className="h-8" />
             </div>
 
-            <div className={ error.length !== 0 || error !== '' ? 'absolute top-20 w-full' : 'hidden'}>
+            <div className={ 
+                error.length !== 0 || error !== '' 
+                    ? 'absolute top-20 w-full' 
+                    : 'hidden'
+                }
+            >
                 <h1 className="text-yellow-400 text-sm md:text-lg text-center">
                     { error }
                 </h1>
