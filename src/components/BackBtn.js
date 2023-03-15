5ae3603239eb084b781d3f10ed6c323beffbdc89
Reydescel Herrera <reydi1@hotmail.com>
2023-03-15 15:15:41 -0600

    Adding Back Btn Component

---
+++ src/components/BackBtn.js
@@ -0,0 +1,22 @@
+import { useNavigate } from 'react-router-dom';
+
+import { ChevronLeftIcon } from '@heroicons/react/24/solid';
+
+function BackBtn() {
+    const navigate = useNavigate();
+
+    const handleGoTo = () => {
+        navigate('/');        
+    }
+
+    return (
+        <div 
+            onClick={ handleGoTo }
+            className="flex items-center justify-center h-10 py-7 text-white w-1/4"
+        >
+            <ChevronLeftIcon className="h-8 w-8" />
+        </div>
+    );
+}
+
+export default BackBtn;
