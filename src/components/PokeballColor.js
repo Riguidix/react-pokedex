94e56ac4af452247ca16c0b5d468e298334b9f65
Reydescel Herrera <reydi1@hotmail.com>
2023-03-15 14:53:21 -0600

    Adding PokeballColor Component

--- src/components/PokeballColor.js
+++ src/components/PokeballColor.js
@@ -2,14 +2,15 @@ import generateColor from '../helpers/ColorGenerator';
 import PokeballImage from '../images/pokeball_transparent.png';
 
 function PokeballColor(props) {
-    let { style, color } = generateColor(props.type);
+    let { style } = generateColor(props.type);
     const containerStyle = "Pokeball absolute flex items-center justify-center";
 
     return (
         <div className={ containerStyle + ' ' + props.layout}>
             <img
+                alt="Pokeball Colored Vector"
+                className="w-3/4 md:w-2/4 lg:w-2/5 z-0"
                 src={ PokeballImage }
-                className="opacity-20 w-3/4 md:w-2/4 lg:w-2/5"
                 style={ { filter: style } }
             />
         </div>
