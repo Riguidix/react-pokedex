6aec153b66d062c8e4c322f709229155d6a0361d
Reydescel Herrera <reydi1@hotmail.com>
2023-03-15 14:50:21 -0600

    Adding Pokeball Component

--- src/components/Pokeball.js
+++ src/components/Pokeball.js
@@ -1,13 +1,12 @@
 import PokeballImage from '../images/pokeball.png';
 
 function Pokeball() {
-    const style = "opacity-10 w-3/4 md:w-2/4 lg:w-2/5";
-
     return (
         <div className="Pokeball absolute flex items-center justify-center">
             <img
+                alt="Pokeball Vector"
                 src={ PokeballImage }
-                className={ style }
+                className="opacity-10 w-3/4 md:w-2/4 lg:w-2/5"
             />
         </div>
     );
